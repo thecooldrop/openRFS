844016d8da2b016e0a2c42c39490435c770c6b52
Vanio Begic <vanio.begic123@gmail.com>
2019-11-18 20:12:01 +0100

    Completed the interface equalization for Kalman filters.
    Now all the Kalman filters have uniform interface as inherited from Kalman ABC,
    this makes the usage for end user easier.
    
    This is managed by making all of the variables necessary for next step of computation
    as object attributes for Kalman filter. Since different Kalman filters need different
    objects for their computations these can then be stored as attributes.

--- estimation/ukf.py
+++ estimation/ukf.py
@@ -15,7 +15,11 @@ class UKF(Kalman):
                  beta,
                  ket,
                  states=None,
-                 covariances=None):
+                 covariances=None,
+                 innovation=None,
+                 innovation_covariances=None,
+                 kalman_gains=None,
+                 ):
         self._transition_model = transition_model
         self._transition_jacobi = transition_jacobi
         self._transition_noise = transition_noise
@@ -25,60 +29,62 @@ class UKF(Kalman):
         self._alpha = alpha
         self._beta = beta
         self._ket = ket
+        self._innovation = innovation
+        self._innovation_covariances = innovation_covariances
+        self._kalman_gains = kalman_gains
+        self._states = states
+        self._covariances = covariances
 
-        self.states = states
-        self.covariances = covariances
+        self._mean_measurements = None
 
     def predict(self):
-        dim = self.states.shape[1]
+        dim = self._states.shape[1]
         lmbd = self._alpha ** 2 * (dim + self._ket) - dim
 
         sigma_points = self._compute_sigma_points(lmbd, dim)
         c_weights, m_weights = self._compute_sigma_weights(lmbd, dim)
 
         predictions = self._transition_model(sigma_points)
-        self.states = np.sum(m_weights[:, np.newaxis] * predictions, axis=1)
+        self._states = np.sum(m_weights[:, np.newaxis] * predictions, axis=1)
 
-        deviation = predictions - self.states[:, np.newaxis]
+        deviation = predictions - self._states[:, np.newaxis]
         scaled_deviation = c_weights[:, np.newaxis] * deviation
-        self.covariances = np.sum(scaled_deviation[:, :, :, np.newaxis] @ deviation[:, :, np.newaxis],
-                                  axis=1) + self._transition_noise
+        self._covariances = np.sum(scaled_deviation[:, :, :, np.newaxis] @ deviation[:, :, np.newaxis],
+                                   axis=1) + self._transition_noise
 
     def update(self, measurements):
-        mean_measurement, innovation_covariances, _, kalman_gain = self.compute_update_matrices()
-        innovation = measurements - mean_measurement
-        self.pure_update(innovation, innovation_covariances, kalman_gain)
+        self.compute_update_matrices()
+        self._innovation = measurements - self._mean_measurements
+        self.pure_update()
 
     def compute_update_matrices(self):
-        dim = self.states.shape[1]
+        dim = self._states.shape[1]
         lmbd = self._alpha ** 2 * (dim + self._ket) - dim
 
         sigma_points = self._compute_sigma_points(lmbd, dim)
         c_weights, m_weights = self._compute_sigma_weights(lmbd, dim)
 
         expected_measurements = self._measurement_model(sigma_points)
-        mean_measurement = np.sum(m_weights[:, np.newaxis] * sigma_points, axis=1)
+        self._mean_measurements = np.sum(m_weights[:, np.newaxis] * sigma_points, axis=1)
 
-        deviation = expected_measurements - mean_measurement[:, np.newaxis]
+        deviation = expected_measurements - self._mean_measurements[:, np.newaxis]
         scaled_deviation = c_weights[:, np.newaxis] * deviation
-        innovation_covariances = np.sum(scaled_deviation[:, :, :, np.newaxis] @ deviation[:, :, np.newaxis],
-                                        axis=1) + self._measurement_noise
+        self._innovation_covariances = np.sum(scaled_deviation[:, :, :, np.newaxis] @ deviation[:, :, np.newaxis],
+                                              axis=1) + self._measurement_noise
 
-        sigma_deviation = sigma_points - self.states[:, np.newaxis]
+        sigma_deviation = sigma_points - self._states[:, np.newaxis]
         scaled_sigma_deviation = c_weights[:, np.newaxis] * sigma_deviation
         kalman_factor = np.sum(scaled_sigma_deviation[:, :, :, np.newaxis] @ sigma_deviation[:, :, np.newaxis], axis=1)
 
-        inv_inno = np.linalg.inv(innovation_covariances)
-        kalman_gain = kalman_factor @ inv_inno
-        return mean_measurement, innovation_covariances, inv_inno, kalman_gain
+        inv_inno = np.linalg.inv(self._innovation_covariances)
+        self._kalman_gains = kalman_factor @ inv_inno
 
-    def pure_update(self,
-                    innovation,
-                    kalman_gain,
-                    innovation_covariances=None):
-        self.states = self.states + np.squeeze(innovation[:, np.newaxis] @ np.transpose(kalman_gain, (0, 2, 1)))
-        self.covariances = self.covariances - \
-                              kalman_gain @ innovation_covariances @ np.transpose(kalman_gain, (0, 2, 1))
+    def pure_update(self):
+        self._states = self._states + np.squeeze(
+            self._innovation[:, np.newaxis] @ np.transpose(self._kalman_gains, (0, 2, 1)))
+        self._covariances = self._covariances - \
+                            self._kalman_gains @ self._innovation_covariances @ np.transpose(self._kalman_gains,
+                                                                                             (0, 2, 1))
 
     def _compute_sigma_weights(self, lmbd, n):
         weight: float = lmbd / (n + lmbd)
@@ -90,9 +96,9 @@ class UKF(Kalman):
         return c_weights, m_weights
 
     def _compute_sigma_points(self, lmbd, n):
-        root_covariances = np.linalg.cholesky((lmbd + n) * self.covariances)
+        root_covariances = np.linalg.cholesky((lmbd + n) * self._covariances)
         # To each state we compute a matrix of sigma points
-        sig_first = self.states[:, np.newaxis] + np.transpose(root_covariances, (0, 2, 1))
-        sig_second = self.states[:, np.newaxis] - np.transpose(root_covariances, (0, 2, 1))
-        sigma_points = np.concatenate((self.states[:, np.newaxis], sig_first, sig_second), axis=1)
+        sig_first = self._states[:, np.newaxis] + np.transpose(root_covariances, (0, 2, 1))
+        sig_second = self._states[:, np.newaxis] - np.transpose(root_covariances, (0, 2, 1))
+        sigma_points = np.concatenate((self._states[:, np.newaxis], sig_first, sig_second), axis=1)
         return sigma_points
